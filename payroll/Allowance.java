be3940b875b2f73a80cf576d7188986424a09382
vathana <136918722+Vathanavaradharaj@users.noreply.github.com>
2024-11-09 14:58:32 +0530

    Update Allowance.java

--- payroll/Allowance.java
+++ payroll/Allowance.java
@@ -6,15 +6,10 @@ import java.util.Scanner;
 public class Allowance {
 	
 	  public int calculate(Scanner in) {
-		 // Scanner scan = new Scanner(System.in);
-		
-	      
-	        System.out.println("Do you come by your own vehicle?(Yes/No):");
+		  System.out.println("Do you come by your own vehicle?(Yes/No):");
 	        String ans = in.nextLine();
-	      //  in.nextLine();
 	        if (!ans.equals("yes") && !ans.equals("no")) {
 	            System.out.println("Invalid input. Please enter 'Yes' or 'No'.");
-	              // Recursive
 	            return calculate(in); 
 	        }
               int travel =0 ;
@@ -34,36 +29,7 @@ public class Allowance {
 	            	}
 	               return travel; 
 	            }
-	        //else {
-	          //  	OverTime(in);
-	            //}
 			return travel;
-	        
-	        
-	    	//	System.out.println("How far u are from?");
-//	    		HashMap<Integer,Object> dis=new HashMap<>();
-//	    		 System.out.println("How far do you live from the office?");
-//		            System.out.println("| Distance | Allowance (Rs) |");
-//		            System.out.println("|   5 km   |      3000      |");
-//		            System.out.println("|  10 km   |      3500      |");
-//		            System.out.println("|  15 km   |      4000      |");
-//		            System.out.println("|  20 km   |      4400      |");
-//	    		dis.put( 5,3000);
-//	    		dis.put(10,3500);
-//	    		dis.put(15,4000);
-//	    		dis.put(20,4400);
-//	    		 System.out.print("Enter the distance in km: ");
-//	    	        int distance = in.nextInt();
-//	    	        in.nextLine();
-//	    	        if(dis.containsKey(distance)) {
-//	    	        	System.out.println("Your Fuel Allowance is Rs."+dis.get(distance)+" per month");
-//	    	        	
-//	        }
-//	    	        else {
-//	    	            System.out.println("We can't afford to pay you, so you might consider working from home instead.");
-//	    	        }
-//	        }else {
-//	        	System.out.print("");
 	        } 
 
 	  public int OverTime(Scanner in) {
